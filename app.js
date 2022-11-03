ed41e75ff6dec8c6223b89b40ef3aa581ea4eb87
Brahian Arias <barsvictorious@gmail.com>
2022-11-03 18:18:34 -0500

    Se implementó la página de Quienes Somos

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
+// Cambio de Brahian
 function info_brahian(){
-    var info = document.getElementById('brahian');
-    info.innerHTML = '<h2>Brahian Arias</h2><p>Le gusta los videojuegos, la cocina, la música y el arte</p><p>Su sueño es ser ingeniero electrónico</p>'
+    let info = document.getElementById('brahian');
+    info.innerHTML = '<h2>Brahian Arias</h2><p>Le gusta los videojuegos, la cocina, la música y el arte</p><p>Su sueño es ser ingeniero electrónico</p>';
 }
 function volver_estado_brahian(){
-    var info = document.getElementById('brahian');
-    info.innerHTML = '<img src="images/avatar/Brahian.png" alt="Avatar de brahiangod" width="300px" height="300px"><h2>Brahian Arias</h2><p>Programador y diseñador</p><p>16 años</p>'
+    let info = document.getElementById('brahian');
+    info.innerHTML = '<img src="images/avatar/Brahian.png" alt="Avatar de brahiangod" width="300px" height="300px"><h2>Brahian Arias</h2><p>Programador y diseñador</p><p>16 años</p>';
+}
+
+// Cambio de Frank
+function info_frank(){
+    let info = document.getElementById('frank');
+    info.innerHTML = '<h2>Frank<br>Rivas</h2><p>Le gusta jugar basquet</p><p>Su sueño es ser ingeniero de sistemas</p>';
+}
+function volver_estado_frank(){
+    let info = document.getElementById('frank');				
+    info.innerHTML = '<img src="images/avatar/Frank.png" alt="Avatar de Frank" width="300px" height="300px"><h2>Frank<br>Rivas</h2><p>Programador</p><p>16 años</p>';
+}
+
+// Cambio de Carlos
+function info_carlos(){
+    let info = document.getElementById('carlos');
+    info.innerHTML = '<h2>Carlos Esteban</h2><p>Le gusta leer y escuchar música</p><p>Su sueño es ir al extranjero</p>';
+}
+function volver_estado_carlos(){
+    let info = document.getElementById('carlos');
+    info.innerHTML = '<img src="images/avatar/Carlos.png" alt="Avatar de Carlos" width="300px" height="300px"><h2>Carlos Esteban</h2><p>Logística</p><p>15 años</p>';
+}
+
+// Cambio de Julian
+function info_julian(){
+    let info = document.getElementById('julian');
+    info.innerHTML = '<h2>Julián Cuervo</h2><p>Le gusta el futbol y el dinero</p><p>Su sueño es emprender una empresa grande</p>';
+}
+function volver_estado_julian(){
+    let info = document.getElementById('julian');
+    info.innerHTML = '<img src="images/avatar/Julian.png" alt="Avatar de Julian" width="300px" height="300px"><h2>Julian Cuervo</h2><p>Comediante</p><p>15 años</p>'
 }
