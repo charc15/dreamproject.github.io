7ce60b6bff028d3402422a0b18cd83ce5a1df5fb
Brahian Arias <barsvictorious@gmail.com>
2022-11-03 06:56:07 -0500

    Se inicia el repositorio con la estructura básica

---
+++ app.js
@@ -0,0 +1,8 @@
+function info_brahian(){
+    var info = document.getElementById('brahian');
+    info.innerHTML = '<h2>Brahian Arias</h2><p>Le gusta los videojuegos, la cocina, la música y el arte</p><p>Su sueño es ser ingeniero electrónico</p>'
+}
+function volver_estado_brahian(){
+    var info = document.getElementById('brahian');
+    info.innerHTML = '<img src="images/avatar/Brahian.png" alt="Avatar de brahiangod" width="300px" height="300px"><h2>Brahian Arias</h2><p>Programador y diseñador</p><p>16 años</p>'
+}
